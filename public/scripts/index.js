dd8ae161ded46c55be3ce9a4b6106db5d9c7b284
Titouan Lacombe--Fabre <titouan.lacombe---fabre@etu.univ-cotedazur.fr>
2025-11-21 10:52:59 +0100

    feat: Implement game pricing rating heatmap visualization.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,9 @@
 import pageController from "./page-controller.js";
-import ratingByGameGenres from "./rating-by-game-genres.js";
+import ratingByGamePricing from "./rating-by-game-pricing.js";
 
 pageController();
 
 // Load data and run the visualization
 const data = await d3.csv('./data.csv');
 
-ratingByGameGenres(data);
+ratingByGamePricing(data);
