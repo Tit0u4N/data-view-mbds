7b00510c5fc852c4d1fc81ca56097a5232cbbc82
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2025-12-06 14:10:25 +0100

    Remove not used cols

--- scripts/filter-data.R
+++ scripts/filter-data.R
@@ -2,8 +2,8 @@
 input_csv <- "scripts/gog_games_dataset.csv"              # Chemin du fichier CSV d'entrée
 output_csv <- "scripts/gog_games_dataset_v2.csv"            # Chemin du fichier CSV de sortie
 cols_to_keep <- c("id", "title", "genres", "dateGlobal", "category",
-                  "salesVisibility", "isFree", "currency", "amount",
-                  "baseAmount", "overallAvgRating")       # Liste des colonnes à conserver
+                  "isFree", "amount", "baseAmount",
+                  "overallAvgRating", "supportedOperatingSystems", "type")       # Liste des colonnes à conserver
 
 
 data <- read.csv(input_csv, stringsAsFactors = FALSE)
@@ -16,7 +16,7 @@ cols_found <- intersect(cols_to_keep, colnames(data))
 cols_missing <- setdiff(cols_to_keep, colnames(data))
 
 if (length(cols_missing) > 0) {
-  warning(paste("Col didn't found in the CSV :", paste(colonnes_manquantes, collapse = ", ")))
+  warning(paste("Col didn't found in the CSV :", paste(cols_missing, collapse = ", ")))
 }
 
 # remove the lines when one of these columns is NA or empty
